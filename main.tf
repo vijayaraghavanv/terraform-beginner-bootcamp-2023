6a32d4f5ca7507475772362b214403b44c54b94b
Vijayaraghavan Vashudevan <vijayaraghavan89@gmail.com>
2023-09-30 09:00:04 +0530

    #21 change structure of root module and tagging added (#22)

--- main.tf
+++ main.tf
@@ -1,40 +1,3 @@
-terraform {
-  #  backend "remote" {
-  #   hostname = "app.terraform.io"
-  #   organization = "TerraformBeginnerBootcamp"
-
-  #   workspaces {
-  #     name = "terra-house-1"
-  #   }
-  # }
- cloud {
-    organization = "TerraformBeginnerBootcamp"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-}
-
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "aws"{
-}
-
-#https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-provider "random" {
-  # Configuration options
-}
-
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "random_string" "bucket_name" {
   lower=true
@@ -48,8 +11,10 @@ resource "aws_s3_bucket" "example" {
   # Bucket Naming Rules
   #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = random_string.bucket_name.result
+
+  
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
-}
