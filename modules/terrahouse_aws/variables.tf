14a6d15ff022c23f648d46ff254e1ca9d7ca5a16
Vijayaraghavan Vashudevan <vijayaraghavan89@gmail.com>
2023-10-15 07:57:14 +0000

    #50 testing our home on terratowns

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -7,18 +7,18 @@ variable "user_uuid" {
   }
 }
 
-variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
-
-  validation {
-    condition     = (
-      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 && 
-      can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.bucket_name))
-    )
-    error_message = "The bucket name must be between 3 and 63 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
-  }
-}
+#variable "bucket_name" {
+#  description = "The name of the S3 bucket"
+#  type        = string
+#
+#  validation {
+#    condition     = (
+#      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 && 
+#      can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.bucket_name))
+#    )
+#    error_message = "The bucket name must be between 3 and 63 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
+#  }
+#}
 
 variable "index_html_filepath" {
   description = "The file path for index.html"
@@ -47,7 +47,7 @@ variable "content_version" {
   validation {
     condition     = var.content_version > 0 && floor(var.content_version) == var.content_version
     error_message = "The content_version must be a positive integer starting at 1."
-  }  
+  }
 }
 
 variable "assets_path" {
